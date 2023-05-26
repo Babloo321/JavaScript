9014b4287c10c37fa22d0c331972100b4e71223d
Babloo Kumar <babloocse888@gmail.com>
2023-05-26 09:04:15 +0530

    second commit

--- index.js
+++ index.js
@@ -18,3 +18,37 @@
 var num = 200;
 console.log(`half of ` + num +` is: ` + num/2);
 console.log(`halt of ${num} is ${num/2}`);
+
+
+// var keyword
+var a = 2333;
+console.log(a);
+var a = "ram";      // it working fine because var support reinitilize as well as redeclearation
+console.log(a);
+// let keyword
+
+let l = 100;
+console.log(l);
+// let l = "babloo"; 
+// getting error--> syntax error< l has been be already decleared
+// console.log(l);
+
+// we can reinitilize but can't redecleared
+l = "babloo";
+console.log(l);
+
+// const keyword
+const i = 3;
+console.log(i);
+// i = 4;           it can't reinitilize and redeclearation //getting error
+
+
+// loops and condition in loop
+num = 10;
+for(let i = 0; i <= num; i++){
+    if(i % 2 == 0){
+        console.log(i + ` is even number`);
+    } else{
+        console.log(i + ` is odd number`);
+    }
+}

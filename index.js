40aa143cccccadb35b2bc28095c4c559d258cdf8
Babloo Kumar <babloocse888@gmail.com>
2023-05-26 08:23:52 +0530

    start java script

---
+++ index.js
@@ -0,0 +1,20 @@
+// var a = 10;
+// console.log(a);
+// a = "summer";
+// console.log(a);
+// console.log('type of ' + a + " is " +typeof a);
+// a = true;
+// console.log('type of :' + a + ' is: ' + typeof a);
+// a = false;
+// console.log('type of :' + a + ' is: ' + typeof a);
+// a = 1;
+// while(a <= 10){
+//     console.log(a + "type of a is: " + typeof a);
+//     a++;
+// }
+// a = 45.53;
+// console.log(typeof a);
+// console.log(a + ` is ` + typeof a + ` type`);
+var num = 200;
+console.log(`half of ` + num +` is: ` + num/2);
+console.log(`halt of ${num} is ${num/2}`);

56a27a7858377dcd2cce3263e59760aa51aa355d
Babloo Kumar <babloocse888@gmail.com>
2023-05-27 08:10:17 +0530

    types of function 3.IIFE-->Imediately Invoke Fucntion Expression

--- function.js
+++ function.js
@@ -36,21 +36,20 @@ function calculater(str,a, b){
     //     let x = d / a;
     //     let y = d / b;
     //     let n = x + y;
-    //     return simplyfy(n, d);
+    //     return function(){
+    //         let gcd = 1;
+    //         let j = Math.min(n, d);
+    //         for(let i = 1; i <= j; i++){
+    //             if(n%i == 0 && d % i == 0){
+    //                 gcd = i;
+    //             }
+    //         }
+    //         n = n / gcd;
+    //         d = d/ gcd;
+    //         console.log("lcm : " + n + '/' +d);
+    //     }
     // }
 }
-// function simplyfy(n, d){
-//     let gcd = 1;
-//     let j = min(n,d);
-//     for(let i = 1; i <= j; i++){
-//         if(n % i == 0 && n % i == 0){
-//             gcd = i;
-//         }
-//     }
-//     n = n /gcd;
-//     d = d / gcd;
-//     return (`${n} / ${d}`);
-// }
 let addition = calculater(`add`, 3,5);
 console.log(`addition of 3 and 5 is ${addition}`);
 let substraction = calculater('sub', 9, 5);
@@ -60,9 +59,8 @@ console.log(`multiplication of 3, 5 is${multiplication}`);
 let division = calculater('div', 124, 4);
 console.log(`vidision of 124 and 4 is ${division}`);
 // let longestCommonFactor = calculater('lcm', 4,6);
-// console.log(`LCM of 4 and 6 is ${longestCommonFactor} `);
+// longestCommonFactor();
 
-*/
 
 
 function calculater(input, a, b){
@@ -77,9 +75,10 @@ function calculater(input, a, b){
 let returnFun = calculater('add', 4, 6);
 console.log("return function is :\n" + returnFun);
 returnFun();
-
+*/
 
 /* function expression:-> in function expression we can return whole function */
+/*
 let funExpression = function expression(){
     console.log("hello guy's i'm function expression");
 }
@@ -90,3 +89,13 @@ function abcd(){
 }
 console.log(" "+ abcd);
 abcd();
+*/
+
+
+// IIFE:-->> Immediate Invoke Function Expression
+
+let addFunction = (function(a, b){
+    return a+b;
+    // console.log(a+b);
+})(33,44);
+console.log(addFunction);

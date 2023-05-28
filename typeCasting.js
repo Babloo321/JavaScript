101f44c8cf464b238c36abd6bf7028c133a441ca
Babloo Kumar <babloocse888@gmail.com>
2023-05-29 01:31:52 +0530

    complete typecasting

---
+++ typeCasting.js
@@ -0,0 +1,10 @@
+var ans = "5" + 1;  //"+" it concatinate the value
+var ans = 1 + "5";
+console.log(ans);
+
+console.log(null + 4);      // null converted to 0 so output is 4
+console.log(undefined * 4); // NaN
+console.log(null * 5);      // 0
+console.log("5" - 1);       // 5-1-> 4
+console.log("ten" * 3);     // string * num (non sensical) NaN
+console.log("10" * 3)       // 30 (10 is number)
